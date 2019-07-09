6d90589a06670cbf964e7ec6bf9b203169492388
SupremeTechnopriest <randylebeau@gmail.com>
2019-07-08 20:22:14 -0700

    ✨ Use handlebars syntax.

--- _engine/modngn.server.config.js
+++ _engine/modngn.server.config.js
@@ -2,7 +2,7 @@ import { join } from 'path'
 import PublicFilesPlugin from '@modngn/plugin-public-files'
 
 export default {
-  modLanguage: '<%= format %>',
+  modLanguage: '{{format}}',
   configFolder: join(__dirname, '../configs'),
   vanillaFolder: join(__dirname, './common/vanilla'),
   modsFolder: join(__dirname, './public'),
